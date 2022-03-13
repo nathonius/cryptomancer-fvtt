71e8f54811029bbddc5bc59b977b8dc9576201e3
OfficerHalf <nathan@nathan-smith.org>
2022-03-12 23:41:25 -0500

    feat: add toggle box component, something is wrong with the skill-list component

--- src/helpers/templates.ts
+++ src/helpers/templates.ts
@@ -7,13 +7,14 @@ export const preloadHandlebarsTemplates = async function () {
   return loadTemplates([
     // Actor partials.
     "systems/cryptomancer/actor-sheet/components/core-triad.component.html",
+    "systems/cryptomancer/actor-sheet/components/skill-list.component.html",
     "systems/cryptomancer/actor-sheet/parts/actor-features.html",
     "systems/cryptomancer/actor-sheet/parts/actor-items.html",
     "systems/cryptomancer/actor-sheet/parts/actor-spells.html",
     "systems/cryptomancer/actor-sheet/parts/actor-effects.html",
-    "systems/cryptomancer/actor-sheet/parts/actor-attribute.html",
     "systems/cryptomancer/skill-check/skill-check.component.html",
     "systems/cryptomancer/shared/components/text-input/text-input.component.html",
     "systems/cryptomancer/shared/components/core-input/core-input.component.html",
+    "systems/cryptomancer/shared/components/toggle-box/toggle-box.component.html",
   ]);
 };
