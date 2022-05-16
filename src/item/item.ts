9defeaf770243f68c6152ffa231d9636be1966b2
Nathan Smith <nathan@nathan-smith.org>
2022-05-15 23:07:49 -0400

    feat: basic support for chat cards from character sheet

--- src/item/item.ts
+++ src/item/item.ts
@@ -1,5 +1,27 @@
+import { ChatMessageDataConstructorData } from "@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/data/data.mjs/chatMessageData";
+import { CryptomancerActor } from "../actor/actor";
+import { getGame } from "../shared/util";
+
 export class CryptomancerItem extends Item {
   public async showChatMessage(): Promise<void> {
-    // pass
+    const _game = getGame();
+    const _actor: CryptomancerActor | undefined = this.actor || undefined;
+    const _token = _actor?.token || undefined;
+
+    const content = await renderTemplate("systems/cryptomancer/item/chat-card.hbs", {
+      title: this.name,
+      avatar: this.img,
+      description: this.data.data.description,
+    });
+
+    const messageData: ChatMessageDataConstructorData = {
+      user: _game.user?.id,
+      speaker: ChatMessage.getSpeaker({ actor: _actor, token: _token }),
+      type: CONST.CHAT_MESSAGE_TYPES.OTHER,
+      content,
+    };
+
+    ChatMessage.applyRollMode(messageData, _game.settings.get("core", "rollMode"));
+    await ChatMessage.create(messageData);
   }
 }
