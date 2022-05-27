f6df25af397490680e13262aaebe6df4136880b9
Nathan Smith <nathan@nathan-smith.org>
2022-05-27 16:09:34 -0400

    feat: update party cell list for readability

--- src/actor-sheet/actor-sheet.interface.ts
+++ src/actor-sheet/actor-sheet.interface.ts
@@ -1,14 +1,20 @@
 import { CryptomancerActor } from "../actor/actor";
+import { CellType } from "../actor/actor.enum";
 import { Party, Skill } from "../actor/actor.interface";
 import { AttributeBarContext } from "../shared/components/components.interface";
 import { CheckDifficulty } from "../skill-check/skill-check.enum";
 
-export type AugmentedData = ActorSheet.Data & {
-  checkDifficulty: CheckDifficulty;
-  hpAttributeBar: AttributeBarContext;
-  manaAttributeBar: AttributeBarContext;
-  skills: Array<Skill & { core: string; attributeValue: number }>;
-  partyOptions: StoredDocument<CryptomancerActor>[];
-  selectedParty: Party | null;
-  upgradePointsUsed: number;
-};
+export type AugmentedData = ActorSheet.Data &
+  Partial<{
+    // Character Data
+    checkDifficulty: CheckDifficulty;
+    hpAttributeBar: AttributeBarContext;
+    manaAttributeBar: AttributeBarContext;
+    skills: Array<Skill & { core: string; attributeValue: number }>;
+    partyOptions: StoredDocument<CryptomancerActor>[];
+    selectedParty: Party | null;
+    upgradePointsUsed: number;
+    // Party Data
+    riskColor: string;
+    cellTypes: Record<CellType, string>;
+  }>;
