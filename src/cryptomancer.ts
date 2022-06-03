e9f9098ec38064a4b5644b2ef46594a0e2703b90
Nathan Smith <nathan@nathan-smith.org>
2022-06-03 17:43:02 -0400

    fix: correct migration version

--- src/cryptomancer.ts
+++ src/cryptomancer.ts
@@ -198,7 +198,9 @@ Hooks.once("ready", async function () {
   if (!game.user?.isGM) return;
   const alwaysMigrate = (game.modules.get("_dev-mode") as any | undefined)?.api?.getPackageDebugValue("cryptomancer");
   const currentVersion = game.settings.get(SCOPE, "systemMigrationVersion") as string;
-  const NEEDS_MIGRATION_VERSION = "0.5.0";
+  // Migrate if the last installed version is LESS than this value
+  // So when updating this value, it should be set to the NEWEST version
+  const NEEDS_MIGRATION_VERSION = "0.6.1";
   const COMPATIBLE_MIGRATION_VERSION = "0.1.0";
   const needsMigration = !currentVersion || isNewerVersion(NEEDS_MIGRATION_VERSION, currentVersion);
   if (!needsMigration && !alwaysMigrate) return;
